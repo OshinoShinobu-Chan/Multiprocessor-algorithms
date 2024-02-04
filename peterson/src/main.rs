eef691dc4e014aa84f332e7f35262a47594e712d
root <root@Crimmy>
2024-02-04 19:44:25 +0800

    Add Peterson algorithm

---
+++ peterson/src/main.rs
@@ -0,0 +1,44 @@
+struct peterson {
+    flag: [bool; 2],
+    victim: i32,
+}
+
+static mut LOCK: peterson = peterson {
+    flag: [false, false],
+    victim: -1, // -1 means no one is victim
+};
+
+fn lock(id: i32) {
+    unsafe {
+        LOCK.flag[id as usize] = true;
+        LOCK.victim = id;
+        while LOCK.flag[1 - id as usize] && LOCK.victim == id {}
+    }
+}
+
+fn unlock(id: i32) {
+    unsafe {
+        LOCK.flag[id as usize] = false;
+    }
+}
+
+use std::thread;
+
+fn main() {
+    println!("Hello, world!");
+    let mut handles = vec![];
+    for i in 0..2 {
+        let id = i;
+        let handle = thread::spawn(move || {
+            lock(id);
+            println!("Thread {id} start");
+            thread::sleep(std::time::Duration::from_millis(100));
+            println!("Thread {id} end");
+            unlock(id);
+        });
+        handles.push(handle);
+    }
+    for handle in handles {
+        handle.join().unwrap();
+    }
+}
